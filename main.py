2f92233351dd1dd58d300c31a411c88695018d87
mreriksen <eriksen2608@gmail.com>
2024-09-25 09:12:20 +0200

    Base

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from debugpy.server.cli import in_range
+
+#import billede af brættet
+
+
+#import billde af krone vendt på alle måder
+
+#Make tiles size
+#M = im.shape[0]//5
+#N = im.shape[1]//5
+
+
+#tiles = [im[x:x+M,y:y+N] for x in range(0,im.shape[0],M) for y in range(0,im.shape[1],N)]
+
+property = []
+
+#
+
+# 5x5 grid
+for i in range(5):
+    for j in range(5):
+        pass
+
+#
+
+#Add all property scores
+for score in property:
+    score += score
+    print(score)
+
+
+
